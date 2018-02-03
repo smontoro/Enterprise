34653d1adff23af14d7b90822e21bcd1611fc7eb
smontoro <sarahjmontoro@gmail.com>
2018-02-02 18:15:45 -0800

    first commit

---
+++ page-about.php
@@ -0,0 +1,100 @@
+<!--OUR MISSION
+   =========================== -->
+<section>
+   <h3>
+      <strong>Our Mission</strong>
+   </h3>
+
+   <img class="wp-image-1099 size-large alignright" style="width: 50%;" src="http://enterpriseforyouth.org/wp-content/uploads/2016/04/About-Us-Picture-1024x682.jpg" alt="About Enterprise for Youth" width="710" height="473">
+
+   <p style="font-weight: 400;">
+      Enterprise for Youth empowers young people to prepare for and discover career opportunitie.
+   </p>
+   <p style="font-weight: 400;">
+      Youth cultivate their individual interests and potential through training, guidance, and employment experiences in supportive and diverse environments.
+   </p>
+   <p style="font-weight: 400;">
+      Enterprise youth build confidence and skills through workshops, one-on-one guidance, and career exploration. Then they put those new skills into practice through employment and internships.
+   </p>
+   <p style="font-weight: 400;">
+      Learning in a safe, supportive, and welcoming environment, Enterprise for Youth members gain more than valuable skills and training. They acquire confidence. They build trusting relationships with supportive adults and they serve as role models for their peers.
+   </p>
+
+   <blockquote>
+      <cite>
+         “For low income youth, early access to economic opportunity is critical- 43% of Americans raised at the bottom of the income ladder remain stuck there as adults… By 2025 sixty-five percent of the jobs in the United States will require some postsecondary education, training or credential- up from 28% in the 1970s. These heightened expectations mean that it’s more essential than ever for young people to gain work experience and skills today to enable them to compete in the global workforce in the future.”
+      </cite>
+
+      <p> -Expanding Economic Opportunity for Youth through Summer Jobs
+         JP Morgan Chase &amp; Co., 2016</p>
+   </blockquote>
+</section>
+
+<hr>
+
+
+<!--OUR IMPACT
+   ============================ -->
+<section>
+   <!-- Left -->
+   <div style="display:inline-block; width:40%; float:left;">
+      <h3>
+         <strong>Our Impact</strong>
+      </h3>
+
+      <div style="padding: 10px;">
+         <p>A job during high school can put a young person on the path to a fulfilling future. Multiple research studies show that:</p>
+
+         <ul>
+            <li>Young people who work during high school are more responsible and have better time and financial management skills.</li>
+            <br>
+            <li>A job in high school has a positive impact on future earnings and occupational status.</li>
+            <br>
+            <li>There is a strong link between working during high school and attaining a job right after graduation.</li>
+            </br>
+            <li>Low income youth who work a moderate amount while in high school are 50% more likely to graduate high school.</li>
+         </ul>
+
+         <div style="text-align: center;">
+            <p>
+               *Source: Long term impacts study by Natalia Van Doren, U.C. Berkeley, July 2015
+               <br>
+               Click to download the reports: 
+            </p>
+
+            <a href="http://enterpriseforyouth.org/wp-content/uploads/2016/06/PathwaysReport_SurveyAnalysis_8-27-15.pdf" target="_blank" rel="noopener noreferrer">Pathways Report Survey Analysis, 8/27/15</a>
+
+            <a href="http://enterpriseforyouth.org/wp-content/uploads/2016/06/TWA-Report-Survey-Analysis-8-10-15.pdf" target="_blank" rel="noopener noreferrer">TWA Report Survey Analysis, 8/10/15</a>
+         </div>
+      </div>
+   </div>
+
+   <!-- Right -->
+   <div style="display:inline-block; width:55%; float:right;">
+      <img class="wp-image-534 size-full" src="http://enterpriseforyouth.org/wp-content/uploads/2016/06/enterprise_job_skills_graph.jpg" alt="Job Skills" width="649" height="299">
+
+      <img class="wp-image-537 size-full" src="http://enterpriseforyouth.org/wp-content/uploads/2016/06/enterprise_future_prospects_graph.jpg" alt="Future Prospects" width="649" height="299">
+   </div>
+
+</section>
+
+
+<!--
+<blockquote>
+<cite style="display:inline-block; width:50%;">“I have always been extremely grateful for Enterprise and visited often to get various jobs to help live a better life during my high school years. I worked all through high school which prepared me to pay my way through college.”</cite>
+
+-Donna Bland, President and CEO Golden 1 Credit Union
+Enterprise participant 1980-1984</blockquote>-->
+
+
+
+<!--HISTORY
+   ====================== -->
+<div>
+      <h3>
+         <strong>History</strong>
+      </h3>
+
+
+   <img class="wp-image-2508 aligncenter" src="http://enterpriseforyouth.org/wp-content/uploads/2017/12/Glady_ThankYou-791x1024.jpg" alt="" width="601" height="778">
+</div>
